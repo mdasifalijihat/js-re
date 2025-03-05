33694bb296edd995a64f859fb1b3a337c57a7cda
mdasifalijihat <mdasifalijihat@gmail.com>
2025-03-05 23:45:15 +0600

    deleted

--- js/arrayMethods.js
+++ js/arrayMethods.js
@@ -1,17 +1,21 @@
 const products = [
-    {id: 1, name: "iphone", color: "black", price: 12000, brand: "apple"},
-    {id: 2, name: "xiaomi", color: "gold", price: 6540, brand: "sam"},
-    {id: 3, name: "samsung", color: "white", price: 812000, brand: "samsung"}
+    { id: 1, name: "iphone", color: "black", price: 12000, brand: "apple" },
+    { id: 2, name: "xiaomi", color: "gold", price: 6540, brand: "sam" },
+    { id: 3, name: "samsung", color: "white", price: 812000, brand: "samsung" }
 ]
 
 
-// filter 
+// delete 
+const newProducts = products.filter(p => p.id != 3);
+console.log(newProducts)
+
+// filter
 // const newProducts = products.filter(product => product.price>10000)
 // console.log(newProducts)
 
-//find 
-const newProducts = products.find(product => product.id===3);
-console.log(newProducts)
+//find
+// const newProducts = products.find(product => product.id===3);
+// console.log(newProducts)
 
 // forEach
 
