5a9efa1a344c8d627cac937639002ef13e101762
mdasifalijihat <mdasifalijihat@gmail.com>
2025-03-07 12:13:55 +0600

    add equal value and type chack

---
+++ js/equal.js
@@ -0,0 +1,11 @@
+const num1 = 10;
+const num2 = '10';
+
+// value chack == dubole equal 
+//  type chake === tropol equal 
+
+if(num1 === num2){
+    console.log('this equal value')
+}else{
+    console.log('Noo this is not equal ')
+}
--- js/true.false.js
+++ js/true.false.js
@@ -10,9 +10,14 @@
 //truthy value
 
 //falsy value;
-const nameU = null;
-if(name){
-    console.log('true')
-}else{
-console.log('false')
-}
+// const nameU = null;
+// if(name){
+//     console.log('true')
+// }else{
+// console.log('false')
+// }
+
+// !value ultay fale
+// const name = 'asif';
+// console.log(!name)
+// console.log(!!name)
