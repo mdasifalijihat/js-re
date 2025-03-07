140b41c7cae93d9c974ff8eb890e716894d5636b
mdasifalijihat <mdasifalijihat@gmail.com>
2025-03-07 12:04:34 +0600

    add true false

--- js/null.js
+++ js/null.js
@@ -11,3 +11,5 @@
 //referenceError --Undefiend 
 // function not return --Undefiend 
 // function not set value ---Undefiend
+// function ar vitore console.log and bahireo console.log kore ---Undefiend dekhabe 
+
---
+++ js/true.false.js
@@ -0,0 +1,18 @@
+
+//truthy value
+// const name = 'Asif Ali';
+
+// if(name){
+//     console.log('true')
+// }else{
+// console.log('false')
+// }
+//truthy value
+
+//falsy value;
+const nameU = null;
+if(name){
+    console.log('true')
+}else{
+console.log('false')
+}
